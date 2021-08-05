2929dc62b88c0e5924a66e577f74bb5428238db8
ofiryag <ofiryagoda@gmail.com>
2021-08-05 19:00:52 +0300

    - added main header - added main.c
    - added main header
    - added main.c

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+/* This file contains the main function of the project that checks if there is a file to assemble and assemble each given file. */
+
+#include<main.h>
 int main(int argc, char *argv[]) 
 {
- char vec[] = "abccddeefffggghhhhiiiijjjjj";
- putchar(vec[5]);
- return(1);
+    int filesCounter;
+	/*Is there a file to compile?*/
+	if(argc < 2)
+	{
+		printf("\n Error: no file to assemble \n");
+		exit(0);
+	}
+	/*Compile each file*/
+	for(filesCounter=1;filesCounter<argc;filesCounter++)
+	{
+		first_pass(argv[filesCounter]);
+		free_tables();
+	}	
+	return 0; 
 }
---
+++ main.h
@@ -0,0 +1,9 @@
+/* This file contains the includes of other header files for the main.c file. */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include "first_pass.h"
+#include "tables.h"
+
+#endif
